9451a95755cc130bd27da61768e0cd33627498dd
pranumane96 <Deshmukh!24>
2018-12-11 16:30:04 -0600

    Final Version

--- Scenario1.py
+++ Scenario1.py
@@ -1,8 +1,35 @@
+"""
+Simulation of Project Outsourcing Planning for an organization.
+The program uses basic Monte Carlo simulation techniques.
+
+Part of IS 590PR Final Project
+School of Information Sciences,
+University of Illinois, Urbana - Champaign
+
+Scenario 1: Fixed employees - This scenario uses Monte Carlo simulation
+to estimate the number of projects and organization can complete
+within a year, based on certain existing variables/resources.
+
+The program returns an average estimated value of completed, incomplete
+and in-progress projects after a year, along with a visual distribution.
+"""
+
 import random
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class Project:
+    """
+    A project is an undertaking by the organization, characterised by
+    certain variables defined by the functions in the class: project duration,
+    team size, sensitivity and priority.
+
+    During each simulation/year, a Project Object is created multiple times,
+    with each object having various characteristics defined by randomizing
+    it's properties.
+    """
+
     def __init__(self):
         return
 
@@ -14,69 +41,86 @@ class Project:
         return list([project_duration, team_size, proj_sensitivity, priority])
 
     def project_duration(self):
-        duration = random.randint(12, 16)
-        return duration
+        # Random distribution within 12-16 weeks
+        return random.randint(12, 16)
 
     def team(self):
-        team_size = random.randint(3, 7)
-        return team_size
+        # Random distribution within 3-7 members
+        return random.randint(3, 7)
 
     def sensitivity(self):
-        proj_sens = random.randint(2, 12)
-        return proj_sens
+        # Random distribution within 2-12 weeks
+        return random.randint(2, 12)
 
     def priority(self):
-        proj_priority = random.randint(1, 5)
-        return proj_priority
+        # Random distribution within 1-5
+        return random.randint(1, 5)
+
 
 class Simulation:
+    """
+    Each simulation, is a year defined by 50 working weeks.
+    Number of employees considered are 30.
+    Both weeks and number of employees can be changed as per requirement.
+
+    The simulation is run over 500 times and the results are averaged.
+    """
 
     def __init__(self):
         return
 
     def simulate_year(self):
+        # Initialisation of variables
         completed_projects = 0
         incomplete_projects = 0
         total_weeks = 50
         available_employees = 30
+
+        # Queues to store projects in progress and projects in waiting
         projects_in_working = []
         projects_in_queue = []
 
         for week in range(total_weeks):
-            if len(projects_in_working) > 0:
-                for every in projects_in_working:
-                        every[0] = every[0] - 1
-                        if every[0] == 0:
-                            projects_in_working.remove(every)
+            if len(projects_in_working) > 0:  # If some projects are in the progress queue
+                for every_project in projects_in_working:
+                        every_project[0] = every_project[0] - 1  # Subtract a week from the project duration
+                        if every_project[0] == 0:  # If project duration is 0
+                            projects_in_working.remove(every_project)
                             completed_projects += 1
-                            available_employees += every[1]
+                            available_employees += every_project[1]  # Add the free employees to available employees
 
-            if random.randint(0, 1):
+            if random.randint(0, 1):  # Randomizing the possibility of a project turning up in the week
                 new_project = Project()
                 new_project = new_project.create()
                 self.print_details(new_project)
                 projects_in_queue.append(new_project)
                 projects_in_queue = self.sort_queue(projects_in_queue)
 
-            if len(projects_in_queue) > 0:
+            if len(projects_in_queue) > 0:  # If project are waiting to be addressed
                 for i in projects_in_queue:
                     if available_employees > i[1]:
                         projects_in_queue.remove(i)
                         available_employees -= i[1]
                         projects_in_working.append(i)
-                for i in projects_in_queue:
+                for i in projects_in_queue:  # Handling Project Sensitivity
                     if i[2] == 0:
                         projects_in_queue.remove(i)
                         incomplete_projects += 1
                     else:
                         i[2] -= 1
 
-        incomplete_projects = incomplete_projects + len(projects_in_queue)
+        incomplete_projects += len(projects_in_queue)  # Projects in queue are considered incomplete
         projects_in_progress = len(projects_in_working)
 
         return [completed_projects, incomplete_projects, projects_in_progress]
 
-    def sort_queue(self, qlist):
+    @staticmethod
+    def sort_queue(qlist):
+        """
+        The function is used to sort projects waiting in queue according to their priority
+        :param qlist: Takes the projects in queue list
+        :return: Returns a list sorted on the basis of the projects' priorities
+        """
         for i in range(1, len(qlist)):
             j = i - 1
             nxt_element = qlist[i]
@@ -95,17 +139,18 @@ if __name__ == '__main__':
     summary_df = pd.DataFrame(columns=['Completed', 'Incomplete', 'In Progress'])
     number_of_simulations = 500
     for i in range(number_of_simulations):
-        s = Simulation()
+        s = Simulation()  # Simulating 500 years
         yearwise_stats = s.simulate_year()
         summary_df = summary_df.append({'Completed': yearwise_stats[0],'Incomplete': yearwise_stats[1], \
-                        'In Progress': yearwise_stats[2]}, ignore_index=True)
+                        'In Progress': yearwise_stats[2]}, ignore_index=True)  # Year-wise stats appended to a DataFrame
     print("\n\n---------------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
-    print("Year-wise statistics look like:")
-    averages = summary_df.mean(axis = 0)
+
+    averages = summary_df.mean()
     print(summary_df)
     print("\nOn an average:\n")
     print(averages)
 
+    # Scatter plot of the performance statistics
     colors = ("green", "red", "yellow")
     x = (summary_df['Completed'], summary_df['Incomplete'], summary_df['In Progress'])
 
@@ -115,5 +160,3 @@ if __name__ == '__main__':
     plt.xlabel('Number of projects')
     plt.ylabel('Index values')
     plt.show()
-
-
--- Scenario2.py
+++ Scenario2.py
@@ -1,10 +1,37 @@
+"""
+Simulation of Project Outsourcing Planning for an organization.
+The program uses basic Monte Carlo simulation techniques.
+
+Part of IS 590PR Final Project
+School of Information Sciences,
+University of Illinois, Urbana - Champaign
+
+Scenario 2: Fixed projects - If a company wants to estimate the
+human resources it will need for a year, this scenario is useful.
+We have used Monte Carlo simulation to understand the requirement
+of human resources in order to achieve a company's goal of
+completing 50 (variable) projects in a year.
+
+The program returns average, minimum and maximum number of
+employees required, and plots a scatter plot of the distribution.
+
+"""
+
 import random
 import pandas as pd
 import matplotlib.pyplot as plt
-from math import ceil
 
 
 class Project:
+    """
+     A project is an undertaking by the organization, characterised by
+    certain variables defined by the functions in the class: project duration,
+    team size, sensitivity and priority.
+
+    During each simulation/year, a Project Object is created multiple times,
+    with each object having various characteristics defined by randomizing
+    it's properties.
+    """
     def __init__(self):
         return
 
@@ -16,32 +43,36 @@ class Project:
         return list([project_duration, team_size, proj_sensitivity, priority])
 
     def project_duration(self):
-        duration = random.randint(12, 16)
-        return duration
+        # Random distribution within 12-16 weeks
+        return random.randint(12, 16)
 
     def team(self):
-        team_size = random.randint(3, 7)
-        return team_size
+        # Random distribution within 3-7 members
+        return random.randint(3, 7)
 
     def sensitivity(self):
-        proj_sens = random.randint(2, 12)
-        return proj_sens
+        # Random distribution within 2-12 weeks
+        return random.randint(2, 12)
 
     def priority(self):
-        proj_priority = random.randint(1, 5)
-        return proj_priority
+        # Random distribution within 1-5
+        return random.randint(1, 5)
+
 
 class Simulation:
-
+    """
+    Simulating every year
+    """
     def __init__(self):
         return
 
     def simulate_year(self, completed_projects, last_year_incomplete):
-        total_weeks = 50
-        projects_in_working = last_year_incomplete
+        total_weeks = 50  # 50 Working weeks in a year
+        projects_in_working = last_year_incomplete  # Handling incomplete projects from previous year
         goal = to_be_completed_projects
-        employee_per_week = pd.DataFrame()
-        print("\n\n---------------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
+        employee_per_week = pd.DataFrame()  # A DataFrame to store number of employees required each week
+        print("\n\n-------------------------------------------------------------------------------------------------\
+--------------------------------------------------------------------\n")
         for week in range(total_weeks):
             required_employees = 0
             if len(projects_in_working) > 0:
@@ -50,22 +81,27 @@ class Simulation:
                         if every[0] == 0:
                             projects_in_working.remove(every)
 
-            if random.randint(0, 1) and goal != 0:
+            if random.randint(0, 1) and goal != 0:  # Randomizing the possibility of a project turning up in the week
                 new_project = Project()
                 new_project = new_project.create()
                 projects_in_working.append(new_project)
                 projects_in_working = self.sort_queue(projects_in_working)
                 goal -= 1
 
-            for each in projects_in_working:
-                required_employees += each[1]
+            for each_project in projects_in_working:
+                required_employees += each_project[1]
             print("Required employees for week {0} is {1}".format(week + 1, required_employees))
             employee_per_week = employee_per_week.append({'Requirement': required_employees}, ignore_index=True)
-        return [employee_per_week.loc[:, "Requirement"].mean(), employee_per_week.loc[:, "Requirement"].min()\
-            ,employee_per_week.loc[:, "Requirement"].max(), projects_in_working]
+        return [employee_per_week.loc[:, "Requirement"].mean(), employee_per_week.loc[:, "Requirement"].min(), \
+                employee_per_week.loc[:, "Requirement"].max(), projects_in_working]
 
-
-    def sort_queue(self, qlist):
+    @staticmethod
+    def sort_queue(qlist):
+        """
+        The function is used to sort projects waiting in queue according to their priority
+        :param qlist: Takes the projects in queue list
+        :return: Returns a list sorted on the basis of the projects' priorities
+        """
         for i in range(1, len(qlist)):
             j = i - 1
             nxt_element = qlist[i]
@@ -84,21 +120,22 @@ if __name__ == '__main__':
     for i in range(number_of_simulations):
         s = Simulation()
         yearwise_stats = s.simulate_year(to_be_completed_projects, incomplete)
-        summary_df = summary_df.append({'Average_Team': yearwise_stats[0],'Minimum_Team': yearwise_stats[1],
-                                        'Max_Team': yearwise_stats[2]}, ignore_index=True)
+        summary_df = summary_df.append({'Average_#_of_Employees': yearwise_stats[0],'Minimum_#_of_Employees': yearwise_stats[1], \
+                                        'Maximum_#_of_Employees': yearwise_stats[2]}, ignore_index=True)
         incomplete = yearwise_stats[3]
-    print("\n\n---------------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
-    print("Year-wise statistics look like:")
+    print("\n\n--------------------------------------------------------------------------------------------------------\
+------------------------------------------------------------\n")
     averages = summary_df.mean().round(0)
     print("\nOn an average:\n")
     print(averages)
 
+    # Scatter plot of the performance statistics
     colors = ("green", "red", "yellow")
-    x = (summary_df['Average_Team'], summary_df['Minimum_Team'], summary_df['Max_Team'])
+    x = (summary_df['Average_#_of_Employees'], summary_df['Minimum_#_of_Employees'], summary_df['Maximum_#_of_Employees'])
 
     for x, color in zip(x, colors):
         plt.scatter(x, y=summary_df.index.values, c = color, alpha=0.5)
-    plt.title('Number of employees required to complete 20 projects in a year')
+    plt.title('Number of employees required to complete 40 projects in a year')
     plt.xlabel('Number of employees required per week')
     plt.ylabel('Index values')
     plt.show()
